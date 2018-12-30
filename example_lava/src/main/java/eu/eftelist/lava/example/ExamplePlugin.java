9f9fe1113a22f2b8e0810eb336d3ccfb52b88b77
BuildTools <unconfigured@null.spigotmc.org>
2018-12-30 20:41:17 +0100

    forgotten 1 line to intial commit

--- example_lava/src/main/java/eu/eftelist/lava/example/ExamplePlugin.java
+++ example_lava/src/main/java/eu/eftelist/lava/example/ExamplePlugin.java
@@ -1,12 +1,13 @@
 package eu.eftelist.lava.example;
 
 import eu.eftelist.lava.abstracts.LavaPlugin;
+import eu.eftelist.lava.example.modules.ExampleModule;
 
 public class ExamplePlugin extends LavaPlugin {
 
     @Override
     public void onStartup() {
-        
+        getModules().put("test", new ExampleModule());
     }
 
     @Override
